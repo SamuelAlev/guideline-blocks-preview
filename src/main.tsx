29254c28bec177604f09283241ada158c16ca386
Samuel Alev <samuel.alev@frontify.com>
2022-10-01 19:04:36 +0200

    feat: add v1 prefix

--- src/main.tsx
+++ src/main.tsx
@@ -10,15 +10,21 @@ import ReactDOM from 'react-dom';
 window.React = React;
 window.ReactDOM = ReactDOM;
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 import { Root } from './Root';
+
+const RedirectToV1 = () => {
+    const { search } = useLocation();
+    return <Navigate to={`/v1${search}`} replace />;
+};
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Root />} />
+                <Route path="/v1" element={<Root />} />
+                <Route path="*" element={<RedirectToV1 />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>,
