e78e000c61e137d634432504d4b714c3f67aea4d
SamuelAlev <SamuelAlev@users.noreply.github.com>
2024-06-02 10:45:17 +0000

    deploy: 8b3ab5f1741b31fc17aa2995c81b5c3d1e5b73c5

---
+++ assets/Embed-C-0BIdTg.js
@@ -0,0 +1,2 @@
+import{u as o,i as c,r as d,j as e}from"./vendor-OeD0w95h.js";import{u as m,V as n,C as i}from"./ViewEditToggle-N-VHsqY2.js";const f=()=>{const{setState:a,setCustomFields:r}=m(),s=o(),[t]=c();return d.useEffect(()=>{r(s.customFields),a(s.state)},[s,r,a]),e.jsxs("div",{className:"h-screen select-none flex flex-col",children:[!t.has("readonly")&&e.jsx("div",{className:"flex gap-4 items-center p-2 mb-2 border-b-1 border-b-[#f1f1f1]",children:e.jsx(n,{})}),e.jsx("div",{className:"p-2",children:e.jsx(i,{})})]})};export{f as Embed};
+//# sourceMappingURL=Embed-C-0BIdTg.js.map
